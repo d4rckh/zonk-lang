c1fbf0a48fb3d803f8ecfd0cb03455d63dfd2482
m4d-d0x <m4d-d0x.xxx@gmail.com>
2019-11-07 17:17:15 +0200

    parse some things

--- src/lexer.py
+++ src/lexer.py
@@ -1,4 +1,4 @@
-
+import re
 
 class Lexer(object):
 
@@ -6,4 +6,19 @@ class Lexer(object):
         self.source_code = source_code
 
     def tokenize(self):
-        print('test')
+        
+        tokens = []
+        source_code = self.source_code.split()
+        source_index = 0 # keep track of the word index.
+         
+        while source_index < len(source_code):
+            word = source_code[source_index]
+
+            if word == "var": tokens.append(["VAR_DECLARATION", word]) # variable declaration
+            elif re.match('[a-z]', word) or re.match('[A-Z]', word): tokens.append(['IDENTIFIER', word]) # words
+            elif re.match('[0-9]', word): tokens.append(['INTEGER', word]) # integers
+            elif word in "=/x=-+": tokens.append(['OPERATOR', word]) # operators
+
+            source_index += 1
+        
+        return tokens
--- src/main.py
+++ src/main.py
@@ -6,11 +6,10 @@ def main():
     content = ""
     with open('test.lang', 'r') as file:
         content = file.read()
-    print(content)
 
     # Lexer
     lex = lexer.Lexer(content)
-    lex.tokenize()
+    print(lex.tokenize())
 
 
 main()
