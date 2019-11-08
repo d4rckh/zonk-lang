397ac1273ae3b4c20b9da7db04690dcf92e7a2cf
m4d-d0x <m4d-d0x.xxx@gmail.com>
2019-11-08 14:02:57 +0200

    varObject

--- src/parser.py
+++ src/parser.py
@@ -1,10 +1,11 @@
-
+from Objects.varObject import VariableObject
 
 class Parser(object):
 
     def __init__(self, tokens):
         self.tokens = tokens
         self.token_index = 0
+        self.transpiled_code = ""
 
     def parse(self):
 
@@ -19,9 +20,16 @@ class Parser(object):
 
             self.token_index += 1
 
+        print(self.transpiled_code)
+
     def parse_variable_declaration(self, token_stream):
 
         tokens_checked = 0
+
+        name = ""
+        operator = ""
+        value = ""
+
         for token in range(0, len(token_stream)):
             
             token_type = token_stream[tokens_checked][0]
@@ -31,23 +39,27 @@ class Parser(object):
             if token_type == "STATEMENT_END": break
 
             if token == 0:
-                print('Variable type: ' + token_value)
+                pass
+                #print('Variable type: ' + token_value)
             elif token == 1 and token_type == "IDENTIFIER":
-                print("Variable name: " + token_value)
+                name = token_value
             elif token == 1 and token_type != "IDENTIFIER":
                 print("ERROR: invalid variable name '" + token_value + "'")
                 quit()
             elif token == 2 and token_type == "OPERATOR":
-                print("Assignament Operator: " + token_value)
+                operator = token_value
             elif token == 2 and token_type != "OPERATOR":
                 print("ERROR: invalid asignament operator is missing or invalid, it should be '='")
                 quit()
             elif token == 3 and token_type in ["STRING","INTEGER", "IDENTIFIER"]:
-                print('Variable Value: ' + token_value)
+                value = token_value
             elif token == 3 and token_type not in ["STRING","INTEGER", "IDENTIFIER"]:
                 print('ERROR: invalid variable name')
                 quit()
 
             tokens_checked += 1
         
+        varObj = VariableObject()
+        self.transpiled_code += varObj.transpile(name, operator, value)
+
         self.token_index += tokens_checked
